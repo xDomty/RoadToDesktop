914e391f04c4a4aae7bde753fdd3210eb1955796
xDomty <xDomty@gmail.com>
2025-01-05 13:54:54 +0400

    Optimizing Operations.h And Removing Print Feature From It

--- 7. Algorithms Level 3/main.cpp
+++ 7. Algorithms Level 3/main.cpp
@@ -1,6 +1,15 @@
-#include "Problem3.h"
+#include "HeadersAfterCourse/Operations.h"
+#include "HeadersBeforeCourse/Random.h"
+#include "HeadersBeforeCourse/Print.h"
 int main()
 {
      srand((unsigned)time(NULL));
-     Problem3::MySolution::main();
+     vector <vector<short>> X;
+     Random::Numbers::FillVector2DimensionalArray::VariableType::ShortIntegar(X,3,3,1,100);
+     optional <short> test = OPERATIONS::_2DVectors::SumRows::Specific<short>(X,{1,4},true);
+     if (test == nullopt)
+     {
+          cout << "EWWWWWW";
+     }
+     Print::Vector2D_AsMatrix::VectorType::ShortInteger("\t\t   The 3D MATRIX ",X,"        ",true,3,3);
 }
