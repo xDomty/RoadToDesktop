f249e133bb6628b38258f5a15924eca0d1f6e78d
Domty ;) <xdomty@gmail.com>
2025-03-29 20:16:50 +0400

    Update Problem1.hpp

--- 8. Algorithms Level 4/include/Problem1.hpp
+++ 8. Algorithms Level 4/include/Problem1.hpp
@@ -3,15 +3,9 @@
 using namespace std;
 
 namespace Problem1{
-   namespace MrSolution{
-       string NumberToText(int Number);
-       int ReadNumber();
-       void main();
-   }
 
-   namespace MySolution{
        string AllTheTextToUpper(string S1);
        string NumberToText(int number);
        void main();
-   }
+
 }
