3f19af5ddda4495bba6f69b2a5b964bb5e44087a
Domty ;) <xdomty@gmail.com>
2025-03-29 20:17:46 +0400

    Update Problem1.cpp

--- 8. Algorithms Level 4/src/Problem1.cpp
+++ 8. Algorithms Level 4/src/Problem1.cpp
@@ -1,84 +1,7 @@
 #include "../include/Problem1.hpp"
 
 namespace Problem1 {
-    namespace MrSolution {
-        string NumberToText(int Number)
-        {
-             if (Number == 0) return "zero";
-             
-             if (Number >= 1 && Number <= 19)
-             {
-                 string arr[] = { "","One","Two","Three","Four","Five","Six","Seven", "Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen" };
-                 return arr[Number] + " ";
-             }
 
-             if (Number >= 20 && Number <= 99)
-             {
-                 string arr[] = { "","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety" };
-                 return arr[Number / 10] + " " + NumberToText(Number % 10);
-             }
-
-             if (Number >= 100 && Number <= 199)
-             {
-                 return "One Hundred " + NumberToText(Number % 100);
-             }
-             
-             if (Number >= 200 && Number <= 999)
-             {
-                 return NumberToText(Number / 100) + "Hundreds " +
-                 NumberToText(Number % 100);
-             }
-
-             if (Number >= 1000 && Number <= 1999)
-             {
-                 return "One Thousand " + NumberToText(Number % 1000);
-             }
-
-             if (Number >= 2000 && Number <= 999999)
-             {
-                 return NumberToText(Number / 1000) + "Thousands " +
-                 NumberToText(Number % 1000);
-             }
-
-             if (Number >= 1000000 && Number <= 1999999)
-             {
-                 return "One Million " + NumberToText(Number % 1000000);
-             }
-
-             if (Number >= 2000000 && Number <= 999999999)
-             {
-                 return NumberToText(Number / 1000000) + "Millions " +
-                 NumberToText(Number % 1000000);
-             }
-
-             if (Number >= 1000000000 && Number <= 1999999999)
-             {
-                 return "One Billion " + NumberToText(Number % 1000000000);
-             }
-
-             else
-             {
-                 return NumberToText(Number / 1000000000) + "Billions " +
-                 NumberToText(Number % 1000000000);
-             }
-        }
-
-        int ReadNumber()
-        {
-             int Number;
-             cout << "\nEnter a Number? ";
-             cin >> Number;
-             return Number;
-        }
-
-        void main()
-        {
-             int Number = ReadNumber();
-             cout << NumberToText(Number);
-        }
-    }
-
-    namespace MySolution {
         string AllTheTextToUpper(string S1) {
             for (auto i = 0; i < S1.length(); i++) {
                 S1[i] = toupper(S1[i]);
@@ -149,5 +72,4 @@ namespace Problem1 {
         void main() {
             cout << AllTheTextToUpper(NumberToText(200000));
         }
-    }
 }
